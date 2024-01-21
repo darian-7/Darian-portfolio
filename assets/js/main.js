80d976a21e65fca60defecf7ebb47c0b6b9f1881
Darian Irani <103888778+darian-7@users.noreply.github.com>
2024-01-21 15:38:31 -0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,258 +1,144 @@
 /*
-	Massively by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+    Massively by HTML5 UP
+    html5up.net | @ajlkn
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
 
-	var	$window = $(window),
-		$body = $('body'),
-		$wrapper = $('#wrapper'),
-		$header = $('#header'),
-		$nav = $('#nav'),
-		$main = $('#main'),
-		$navPanelToggle, $navPanel, $navPanelInner;
+    // Variables for various page elements
+    var $window = $(window),
+        $body = $('body'),
+        $wrapper = $('#wrapper'),
+        $header = $('#header'),
+        $nav = $('#nav'),
+        $main = $('#main'),
+        $navPanelToggle, $navPanel, $navPanelInner;
 
-	// Breakpoints.
-		breakpoints({
-			default:   ['1681px',   null       ],
-			xlarge:    ['1281px',   '1680px'   ],
-			large:     ['981px',    '1280px'   ],
-			medium:    ['737px',    '980px'    ],
-			small:     ['481px',    '736px'    ],
-			xsmall:    ['361px',    '480px'    ],
-			xxsmall:   [null,       '360px'    ]
-		});
+    // Breakpoints.
+    breakpoints({
+        default:   ['1681px',   null       ],
+        xlarge:    ['1281px',   '1680px'   ],
+        large:     ['981px',    '1280px'   ],
+        medium:    ['737px',    '980px'    ],
+        small:     ['481px',    '736px'    ],
+        xsmall:    ['361px',    '480px'    ],
+        xxsmall:   [null,       '360px'    ]
+    });
 
-	/**
-	 * Applies parallax scrolling to an element's background image.
-	 * @return {jQuery} jQuery object.
-	 */
-	$.fn._parallax = function(intensity) {
+    // ... (rest of the existing code) ...
 
-		var	$window = $(window),
-			$this = $(this);
+    // Play initial animations on page load.
+    $window.on('load', function() {
+        window.setTimeout(function() {
+            $body.removeClass('is-preload');
+        }, 100);
+    });
 
-		if (this.length == 0 || intensity === 0)
-			return $this;
+    // ... (rest of the existing code) ...
 
-		if (this.length > 1) {
+    // Intro.
+    var $intro = $('#intro');
 
-			for (var i=0; i < this.length; i++)
-				$(this[i])._parallax(intensity);
+    if ($intro.length > 0) {
 
-			return $this;
+        // Hack: Fix flex min-height on IE.
+        if (browser.name == 'ie') {
+            $window.on('resize.ie-intro-fix', function() {
 
-		}
+                var h = $intro.height();
 
-		if (!intensity)
-			intensity = 0.25;
+                if (h > $window.height())
+                    $intro.css('height', 'auto');
+                else
+                    $intro.css('height', h);
 
-		$this.each(function() {
+            }).trigger('resize.ie-intro-fix');
+        }
 
-			var $t = $(this),
-				$bg = $('<div class="bg"></div>').appendTo($t),
-				on, off;
+        // Hide intro on scroll (> small).
+        breakpoints.on('>small', function() {
 
-			on = function() {
+            $main.unscrollex();
 
-				$bg
-					.removeClass('fixed')
-					.css('transform', 'matrix(1,0,0,1,0,0)');
+            $main.scrollex({
+                mode: 'bottom',
+                top: '25vh',
+                bottom: '-50vh',
+                enter: function() {
+                    $intro.addClass('hidden');
+                },
+                leave: function() {
+                    $intro.removeClass('hidden');
+                }
+            });
 
-				$window
-					.on('scroll._parallax', function() {
+        });
 
-						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
+        // Hide intro on scroll (<= small).
+        breakpoints.on('<=small', function() {
 
-						$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
+            $main.unscrollex();
 
-					});
+            $main.scrollex({
+                mode: 'middle',
+                top: '15vh',
+                bottom: '-15vh',
+                enter: function() {
+                    $intro.addClass('hidden');
+                },
+                leave: function() {
+                    $intro.removeClass('hidden');
+                }
+            });
 
-			};
+        });
 
-			off = function() {
+    }
 
-				$bg
-					.addClass('fixed')
-					.css('transform', 'none');
+    // Document ready.
+    $(function() {
 
-				$window
-					.off('scroll._parallax');
-
-			};
-
-			// Disable parallax on ..
-				if (browser.name == 'ie'			// IE
-				||	browser.name == 'edge'			// Edge
-				||	window.devicePixelRatio > 1		// Retina/HiDPI (= poor performance)
-				||	browser.mobile)					// Mobile devices
-					off();
-
-			// Enable everywhere else.
-				else {
-
-					breakpoints.on('>large', on);
-					breakpoints.on('<=large', off);
-
+		// Tab functionality.
+		var tabs = $('#nav .links a');
+		var projects = $('#main article');
+	
+		tabs.on('click', function(e) {
+			var category = $(this).attr('href').replace('#', '');
+	
+			// Check if the 'ALL' tab is clicked, if so show all projects.
+			if (category === 'all') {
+				// If you want to force a full page reload from the server, uncomment the following line:
+				// window.location.href = window.location.pathname + '?upd=' + new Date().getTime();
+				// return; // Exit the function early after forcing a full page reload
+	
+				// Manually show all projects
+				projects.show();
+	
+				// Update active tab styling
+				tabs.parent().removeClass('active');
+				$(this).parent().addClass('active');
+	
+				e.preventDefault();
+				return; // Exit the function early after showing all projects
+			}
+	
+			// Continue with existing code to filter projects for other categories
+			// ... (rest of the existing filtering code) ...
+	
+			// Filter projects for other categories
+			projects.each(function() {
+				if ($(this).data('category') === category) {
+					$(this).show();
+				} else {
+					$(this).hide();
 				}
-
-		});
-
-		$window
-			.off('load._parallax resize._parallax')
-			.on('load._parallax resize._parallax', function() {
-				$window.trigger('scroll');
 			});
-
-		return $(this);
-
-	};
-
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
+	
+			// Update active tab styling
+			tabs.parent().removeClass('active');
+			$(this).parent().addClass('active');
 		});
-
-	// Scrolly.
-		$('.scrolly').scrolly();
-
-	// Background.
-		$wrapper._parallax(0.925);
-
-	// Nav Panel.
-
-		// Toggle.
-			$navPanelToggle = $(
-				'<a href="#navPanel" id="navPanelToggle">Menu</a>'
-			)
-				.appendTo($wrapper);
-
-			// Change toggle styling once we've scrolled past the header.
-				$header.scrollex({
-					bottom: '5vh',
-					enter: function() {
-						$navPanelToggle.removeClass('alt');
-					},
-					leave: function() {
-						$navPanelToggle.addClass('alt');
-					}
-				});
-
-		// Panel.
-			$navPanel = $(
-				'<div id="navPanel">' +
-					'<nav>' +
-					'</nav>' +
-					'<a href="#navPanel" class="close"></a>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'right',
-					target: $body,
-					visibleClass: 'is-navPanel-visible'
-				});
-
-			// Get inner.
-				$navPanelInner = $navPanel.children('nav');
-
-			// Move nav content on breakpoint change.
-				var $navContent = $nav.children();
-
-				breakpoints.on('>medium', function() {
-
-					// NavPanel -> Nav.
-						$navContent.appendTo($nav);
-
-					// Flip icon classes.
-						$nav.find('.icons, .icon')
-							.removeClass('alt');
-
-				});
-
-				breakpoints.on('<=medium', function() {
-
-					// Nav -> NavPanel.
-						$navContent.appendTo($navPanelInner);
-
-					// Flip icon classes.
-						$navPanelInner.find('.icons, .icon')
-							.addClass('alt');
-
-				});
-
-			// Hack: Disable transitions on WP.
-				if (browser.os == 'wp'
-				&&	browser.osVersion < 10)
-					$navPanel
-						.css('transition', 'none');
-
-	// Intro.
-		var $intro = $('#intro');
-
-		if ($intro.length > 0) {
-
-			// Hack: Fix flex min-height on IE.
-				if (browser.name == 'ie') {
-					$window.on('resize.ie-intro-fix', function() {
-
-						var h = $intro.height();
-
-						if (h > $window.height())
-							$intro.css('height', 'auto');
-						else
-							$intro.css('height', h);
-
-					}).trigger('resize.ie-intro-fix');
-				}
-
-			// Hide intro on scroll (> small).
-				breakpoints.on('>small', function() {
-
-					$main.unscrollex();
-
-					$main.scrollex({
-						mode: 'bottom',
-						top: '25vh',
-						bottom: '-50vh',
-						enter: function() {
-							$intro.addClass('hidden');
-						},
-						leave: function() {
-							$intro.removeClass('hidden');
-						}
-					});
-
-				});
-
-			// Hide intro on scroll (<= small).
-				breakpoints.on('<=small', function() {
-
-					$main.unscrollex();
-
-					$main.scrollex({
-						mode: 'middle',
-						top: '15vh',
-						bottom: '-15vh',
-						enter: function() {
-							$intro.addClass('hidden');
-						},
-						leave: function() {
-							$intro.removeClass('hidden');
-						}
-					});
-
-			});
-
-		}
+	});
 
 })(jQuery);
